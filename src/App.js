d3acd87c9247757331092a5d4ddfc09057872e95
kianamay <maykiana@msu.edu>
2024-03-26 09:39:53 -0400

    text js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Kiana
+          Learn React with Kiana and MSU!
         </a>
       </header>
     </div>
